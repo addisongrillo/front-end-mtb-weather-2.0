381bdd22b8145a1bbee66d6523d7b429f6020ef3
Addison Grillo <addisongrillo@gmail.com>
2020-11-01 18:21:51 -0500

    AboutPageAndMobileFixes

--- src/App.js
+++ src/App.js
@@ -260,6 +260,11 @@ function App(props) {
                 </Route>
                 <Route path="/About">
                   <h1>About</h1>
+                  <p>MTB Weather is a mountain biking specific weather app.</p>
+                  <p>Add you favorite Trails and instantly see the upcoming weather forecast so you alwasy know which trail will be dry.</p>
+                  <p>MTB Weather is a Progressive Web App.  This means you can install it directly on your device from the website.</p>
+                  <p>If you are using google chrome, just click the 3 dots in the top right corner, then if you're on a mobile device, select add to home screen.  Otherwise select Install MTB Weather.</p>
+                  <p>MTB Weather uses the OpenWeather API for forecast information as well as the Google Maps API for maps integration and location search.</p>
                 </Route>
                 <Route path="/Login">
                   <SignInForm  Login={Login}  changeLoading={changeLoading}/>
