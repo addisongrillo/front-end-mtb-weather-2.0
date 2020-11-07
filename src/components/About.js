64b2999bbb7a834caf7428dac444d737d1efee34
Addison Grillo <addisongrillo@gmail.com>
2020-11-06 20:51:47 -0500

    spelling

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ function About() {
         <div id='aboutText'>
           <h1>About</h1>
           <p className='apara'>MTB Weather is a mountain biking specific weather app.</p>
-          <p className='apara'>Add you favorite Trails and instantly see the upcoming weather forecast so you alwasy know which trail will be dry.</p>
+          <p className='apara'>Add you favorite trails and instantly see the upcoming weather forecast so you always know which trail will be dry.</p>
           <p className='apara'>You can also view weather for the past 24 hours to see if the trail is still wet from previous rain.</p>
           <p className='apara'>MTB Weather is a Progressive Web App.  This means you can install it directly on your device from the website.</p>
           <p className='apara'>If you are using google chrome, just click the 3 dots in the top right corner, then if you're on a mobile device, select add to home screen.  Otherwise select Install MTB Weather.</p>
